71a40a860e3531e7cd4af45894c243e4d6127a9a
HenryCo <henrycodev@gmail.com>
2019-04-02 16:19:14 +0200

    #Wip

--- src/main/java/dev/tindersamurai/atencjobot/bot/command/NewDayNewMeCommand.java
+++ src/main/java/dev/tindersamurai/atencjobot/bot/command/NewDayNewMeCommand.java
@@ -11,9 +11,28 @@ import net.dv8tion.jda.api.managers.GuildController;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 @Component @Slf4j
 public class NewDayNewMeCommand extends AtencjoBotCommandListener {
+
+	private static final String[] set = {
+			"The", "A", "Un", "In", "Rape", "Epic", "", "xXx", "Robbie", "TRIGGERED",
+			"Barry", "Dat", "Over", "Under", "Inside", "Lil'", "Big", "F", "Baka",
+			"Pepe", "Legend", "Harambe", "MemeQueen", "Kops", "Rotten", "Bee", "Boi",
+			"Watch", "Tale", "Shrek", "Donkey", "Master", "2137", "BENIS", "Hitler",
+			"27", "23", "xXx", "420", "2016", "2017", "Benson", "2019", "2078", "DidnDoNothing",
+			"Waddup", "ing", "Baiter", "NumberOne", "'d", "ddd", "XD", "xd", "Xd"
+	};
+
+	private static String rollName() {
+		val count = new Random().nextInt(2) + 2;
+		val str = new StringBuilder();
+		for (int i = 0; i < count; i++) {
+			str.append(set[new Random().nextInt(set.length)]);
+		}
+		return str.toString().substring(0, Math.min(str.length(), 32));
+	}
 
 	@Override
 	public void onCommand(MessageReceivedEvent event, GuildController controller) {
@@ -21,7 +40,7 @@ public class NewDayNewMeCommand extends AtencjoBotCommandListener {
 		if (list.isEmpty()) list.add(event.getAuthor());
 		for (val user : list) {
 			log.debug("USER TO CHANGE: {}", user);
-			controller.setNickname(event.getGuild().getMember(user), "BINGO").queue();
+			controller.setNickname(event.getGuild().getMember(user), rollName()).queue();
 		}
 	}
 
